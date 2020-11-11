fdc581faae3d903bad876a43ba942494251f5879
z3t4z00k <z3t4z00k@gmail.com>
2020-11-11 11:16:31 -0500

    Added views & methods to add and list all the goals

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+	const [newGoal, setNewGoal] = useState("")
+	const [goals, setGoals] = useState([])
+
+	const goalChangeHandler = (text) => {
+		setNewGoal(text)
+	}
+
+	const addGoalHandler = () => {
+		setGoals(currentGoals => [newGoal, ...currentGoals])
+		setNewGoal("")
+	}
+
+	return (
+		<View style={styles.root}>
+			<View style={styles.addGoals}>
+				<TextInput placeholder="Enter a goal" style={styles.addGoals_Input} onChangeText={goalChangeHandler} value={newGoal}/>
+				<Button title="ADD" style={styles.addGoals_Button} onPress={addGoalHandler}/>
+			</View>
+			<View style={styles.goals}>
+				<View style={styles.heading_secondary_container}>
+					<Text style={styles.heading_secondary}>Goals</Text>
+				</View>
+				{goals.map((goal)=> <View key={goal} style={styles.goals_item}><Text>{goal}</Text></View>)}
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	root: {
+		paddingVertical: 40,
+		paddingHorizontal: 32
+	},
+	heading_secondary_container:{
+		paddingVertical: 10
+	},
+	heading_secondary:{
+		fontSize: 16
+	},
+	addGoals: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		width: "100%"
+	},
+	addGoals_Input: {
+		borderBottomColor: 'black',
+		borderBottomWidth: 1,
+		padding: 10,
+		width: "80%"
+	},
+	addGoals_Button: {
+		width: "20%"
+	}, 
+	goals: {
+		paddingTop: 20,
+		flexDirection: "column",
+		width: "100%"
+	},
+	goals_item:{
+		paddingHorizontal: 10,
+		paddingVertical: 8,
+		borderRadius: 8,
+		backgroundColor: "#efefef",
+		marginVertical: 4
+	}
 });
